f14788c1973ed64424b2279b9cf0644643c5b921
ALEXIMTUC21 <113297850+thefak123@users.noreply.github.com>
2023-05-16 08:05:36 +0700

    Initial Commit

---
+++ ProjectALP_MoneyGenius/ProjectALP_MoneyGeniusApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectALP_MoneyGeniusApp.swift
+//  ProjectALP_MoneyGenius
+//
+//  Created by MacBook Pro on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectALP_MoneyGeniusApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
