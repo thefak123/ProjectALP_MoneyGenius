f61270d97c009d4098db5301a49ac04c8133b1a8
ALEXIMTUC21 <113297850+thefak123@users.noreply.github.com>
2023-06-03 14:33:00 +0700

    Update dari Alex : 1. Update statistics screen untuk handle ketika tidak ada data

--- ProjectALP_MoneyGenius/Screens/StatisticsScreen.swift
+++ ProjectALP_MoneyGenius/Screens/StatisticsScreen.swift
@@ -14,28 +14,33 @@ struct StatisticsScreen: View {
 
     var body: some View {
         VStack(alignment: .center){
-            
-            Chart {
-                ForEach(viewModel.data, id: \.type) { element in
-                    ForEach(element.data) { data in
-                        BarMark(x: .value("Month", data.monthName), y: .value("Total amount", data.totalAmount))
-                        
-                    }.foregroundStyle(by: .value("Type", element.type)).position(by: .value("Type", element.type))
-                }
-            }.frame(height: UIScreen.main.bounds.height / 4)
-            HStack(alignment: .center){
-                MainScreenBlock(image: "square.and.arrow.down", text: "Rp. \(viewModel.totalIncome)", desc: "Income", systemName: true)
-                MainScreenBlock(image: "square.and.arrow.up", text: "Rp. \(viewModel.totalExpense)", desc: "Outcome", systemName: true)
-            }.frame(height: UIScreen.main.bounds.height / 4)
-            Text("Latest Transaction").font(.title).fontWeight(.bold).padding([.top, .bottom], 20)
-                Divider()
-                ForEach(viewModel.latestTransaction, id:\.id){ trans in
-                    TransactionCardComponent(trans: trans)
+            if viewModel.data.count != 0{
+                if viewModel.data[0].data.count != 0{
+                    Chart {
+                        ForEach(viewModel.data, id: \.type) { element in
+                            ForEach(element.data) { data in
+                                BarMark(x: .value("Month", data.monthName), y: .value("Total amount", data.totalAmount))
+                                
+                            }.foregroundStyle(by: .value("Type", element.type)).position(by: .value("Type", element.type))
+                        }
+                    }.frame(height: UIScreen.main.bounds.height / 4)
+                    HStack(alignment: .center){
+                        MainScreenBlock(image: "square.and.arrow.down", text: "Rp. \(viewModel.totalIncome)", desc: "Income", systemName: true)
+                        MainScreenBlock(image: "square.and.arrow.up", text: "Rp. \(viewModel.totalExpense)", desc: "Outcome", systemName: true)
+                    }.frame(height: UIScreen.main.bounds.height / 4)
+                    Text("Latest Transaction").font(.title).fontWeight(.bold).padding([.top, .bottom], 20)
                     Divider()
+                    ForEach(viewModel.latestTransaction, id:\.id){ trans in
+                        TransactionCardComponent(trans: trans)
+                        Divider()
+                        
+                    }
                     
+                    Spacer()
+                }else {
+                    Text("There is no data to display")
                 }
-            
-            Spacer()
+            }
         }.padding(20).onAppear{
             viewModel.getSumTransactionByMonth()
             viewModel.getLatestTransaction(n: 5)
